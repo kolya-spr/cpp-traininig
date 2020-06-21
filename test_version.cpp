e4a170098612fcbc3856e71f42c1ca572f5d3fed
Nikolay L <lna9174@gmail.com>
2020-06-21 23:04:16 +0300

    Output for tests

--- test_version.cpp
+++ test_version.cpp
@@ -6,7 +6,10 @@
 BOOST_AUTO_TEST_SUITE(cpptests_suite)
 
 BOOST_AUTO_TEST_CASE(cpplesson1_test_version){
-    std::cout << CI_BUILD_NUMBER << std::endl;
+    std::cout << "Some text to console output" << std::endl;
+    std::cerr << "Some text to console errors" << std::endl;
+    std::cout << "CI_BUILD_NUMBER from application: " << CI_BUILD_NUMBER << std::endl;
+    std::cout.flush();
     BOOST_CHECK(CI_BUILD_NUMBER > 0);
 }
 
