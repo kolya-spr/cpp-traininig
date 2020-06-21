6c11028f7182241a22b832193eb00fa073c7403e
Nikolay L <lna9174@gmail.com>
2020-06-21 16:38:42 +0300

    Without lib

--- main.cpp
+++ main.cpp
@@ -1,6 +1,12 @@
 #include <iostream>
+#include "version.h"
+
+int version() {
+    return CI_BUILD_NUMBER;
+}
 
 int main() {
+    std::cout << "build " << version() << std::endl;
     std::cout << "Hello, World!" << std::endl;
     return 0;
 }
---
+++ test_version.cpp
@@ -0,0 +1,11 @@
+#define BOOST_TEST_MODULE cpptests
+#include "boost/test/unit_test.hpp"
+#include "version.h"
+
+BOOST_AUTO_TEST_SUITE(cpptests_suite)
+
+BOOST_AUTO_TEST_CASE(cpplesson1_test_version){
+    BOOST_CHECK(CI_BUILD_NUMBER > 0);
+}
+
+BOOST_AUTO_TEST_SUITE_END()
